8a955e648bd32d2fcbd0a87763b281645cc7eea8
FlameFreezing <duong.chieu.nguyen98@gmail.com>
2024-03-21 10:35:28 +0700

    first commit

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="header-container">
     <div class="remaining-coin">
-      <p class="remaining-coin-text">残コイン数</p>
-      <p class="remaining-coin-number">3400pt</p>
+      <div class="back-btn" v-if="isInChatRoom">
+        <BackIcon
+          @click="
+            router.push({
+              name: 'Home'
+            })
+          "
+        />
+      </div>
+      <div class="reamining-coin-content">
+        <p class="remaining-coin-text">残コイン数</p>
+        <p
+          class="remaining-coin-number"
+          :style="{
+            textAlign: isInChatRoom ? 'end' : 'start'
+          }"
+        >
+          3400pt
+        </p>
+      </div>
     </div>
     <div class="buy-coin">
       <div class="buy-coin-btn">コイン購入</div>
@@ -15,8 +33,11 @@
 
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
+import BackIcon from './Icons/BackIcon.vue'
+import { computed } from 'vue'
 
+const route = useRoute()
 const router = useRouter()
 const authStore = useAuthStore()
 
@@ -26,6 +47,8 @@ const logout = async () => {
     name: 'Login'
   })
 }
+
+const isInChatRoom = computed(() => route.name === 'Chat')
 </script>
 
 <style scoped>
@@ -34,11 +57,30 @@ const logout = async () => {
   background: #fff;
 }
 
+.back-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+
+  cursor: pointer;
+}
+
 .remaining-coin {
   padding-left: 10px;
+  padding-right: 10px;
   flex: 1;
   border-right: 2px solid #000;
 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.remaining-coin-content {
   display: flex;
   flex-direction: column;
   justify-content: center;
