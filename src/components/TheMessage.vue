4986e10e1a7e0c897990725457e42f291bd6668e
FlameFreezing <duong.chieu.nguyen98@gmail.com>
2024-03-22 16:13:10 +0700

    add bubble for chat

--- src/components/TheMessage.vue
+++ src/components/TheMessage.vue
@@ -1,8 +1,10 @@
 <template>
   <div
-    class="message-container"
+    class="message-container bubble"
     :class="{
-      own: isOwn
+      own: isOwn,
+      'bubble-bottom-right': isOwn,
+      'bubble-bottom-left': !isOwn
     }"
   >
     {{ message.msg }}
@@ -27,11 +29,48 @@ defineProps<Props>()
   font-size: 14px;
   max-width: 70%;
   white-space: pre-wrap;
+  word-break: break-all;
 }
 
 .own {
   align-self: flex-end;
   background: #5356ff;
   color: #fff;
+}
+
+.bubble {
+  position: relative;
+  font-size: 14px;
+  min-width: 60px;
+  background: #fff;
+  border-radius: 16px;
+  padding: 10px;
+  color: #000;
+}
+
+.bubble-bottom-left:before {
+  content: '';
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border-right: 8px solid transparent;
+  border-left: 12px solid #fff;
+  border-top: 8px solid #fff;
+  border-bottom: 12px solid transparent;
+  left: 24px;
+  bottom: -16px;
+}
+
+.bubble-bottom-right:before {
+  content: '';
+  width: 0px;
+  height: 0px;
+  position: absolute;
+  border-right: 12px solid #fff;
+  border-left: 8px solid transparent;
+  border-top: 8px solid #fff;
+  border-bottom: 12px solid transparent;
+  right: 24px;
+  bottom: -16px;
 }
 </style>
